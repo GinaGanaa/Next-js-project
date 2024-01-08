7f8cb4956733902644c49c2e20e722cadca65f9e
gina <otgoo.gantsetseg@gmail.com>
2024-01-08 13:28:13 -0500

    todo app improvement

--- src/components/todos/AddTodo.jsx
+++ src/components/todos/AddTodo.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import styles from "./AddTodo.module.css";
 import { Racing_Sans_One } from "next/font/google";
 const Racing = Racing_Sans_One({
@@ -7,15 +8,35 @@ const Racing = Racing_Sans_One({
   display: "swap",
 });
 
-export const AddTodo = () => {
+export const AddTodo = (props) => {
+  const [input, setInput] = useState("");
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000),
+    //   text: input,
+    // });
+    setInput("");
+  };
+
   return (
     <>
-      <div className={styles.addInput}>
-        <input type="text" />
+      <form className={styles.addInput} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add todo"
+          value={input}
+          name="text"
+          className="todoInput"
+          onChange={handleChange}
+        />
         <div className={`${Racing.className}`}>
           <button>Add task</button>
         </div>
-      </div>
+      </form>
       <div className={styles.bottomLine}></div>
     </>
   );
