ff498966e6a2a547448f9a6f6170505680c74a16
gina <otgoo.gantsetseg@gmail.com>
2024-01-10 00:16:26 -0500

    todo app improvement

--- src/components/todos/Todo.jsx
+++ src/components/todos/Todo.jsx
@@ -1,33 +1,17 @@
 import { DeleteIcon } from "../icons/DeleteIcon";
-import { v4 as uuidv4 } from "uuid";
 import styles from "./Todo.module.css";
-uuidv4();
 
-// export const Todo = (props) => {
-//   const { data } = props;
+export const Todo = (props) => {
+  const { data } = props;
 
-export const Todo = () => {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (todo) => {
-    setTodos([
-      ...todos,
-      { id: uuidv4(), task: todo, completed: false, isEditing: false },
-    ]);
-  };
   return (
-    <div className="TodoWrapper">
-      <TodoForm addTodo={addTodo} />
+    <div className={styles.inputTask}>
+      <div className={styles.task}>
+        <div>{data.task}</div>
+        <button className={styles.deleteBtn}>
+          <DeleteIcon />
+        </button>
+      </div>
     </div>
   );
-  // return (
-  //   <div className={styles.inputTask}>
-  //     <div className={styles.task}>
-  //       <div>{data.task}</div>
-  //       <button className={styles.deleteBtn}>
-  //         <DeleteIcon />
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
 };
